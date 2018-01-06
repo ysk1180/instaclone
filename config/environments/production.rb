832af0abc717d87a06b120597023f49d95bdc555
自分の名前を入力してください <yusuke@YusukenoMacBook-Air.local>
2018-01-06 15:10:07 +0900

    after configuration of sendgrid 1/6 15:10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 本番環境での確認メール送信設定
+  config.action_mailer.default_url_options = { host: 'boiling-retreat-24849.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.SendGrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
 end
